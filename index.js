6675a5e947f9700deccd4cd3b9bd8191796aafe7
mahadi24t <mahadi24t@outlook.com>
2025-02-25 18:13:00 +0600

    count

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-document.getElementById("count-el").innerText = 6
+// document.getElementById("count-el").innerText = 6
+
+// let count = 0;
+
+// //console.log(count);
+
+// let myAge=25;
+
+// //console.log(myAge);
+
+// // cmd+k+c
+// // let firstBatch = 5
+// // let secondBatch = 7
+// // let count = firstBatch + secondBatch
+// // console.log(count)
+
+// let myAge = 25;
+// let dogratio =7;
+
+// let dogage= myAge*dogratio;
+
+// console.log(dogage);
+
+// function increment() {
+//     console.log("Increment Button was clicked");
+
+// }
+
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// let loglaptime = () => {
+//     let totaltime = lap1 + lap2 + lap3;
+//     console.log(totaltime);
+// };
+
+// loglaptime();
+
+// let lapcompleted = 0;
+
+// let incrementLap = () => {
+//     lapcompleted += 1;
+//     //console.log(lapcompleted);
+// };
+
+// incrementLap();
+// incrementLap();
+// incrementLap();
+// console.log(lapcompleted);
+
+let countEl = document.getElementById('count-el'); //pass in argument
+console.log(countEl);
+let count = 0
+
+function increment() {
+   // console.log("clicked")
+    count=count + 1;
+    countEl.innerText = count;
+    //return count;
+    console.log(count);
+}
